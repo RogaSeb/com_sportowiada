0b8af0c3527c3646c50949b0fa9a85686a8d3737
RogaSeb <rogackis@gmail.com>
2024-10-22 23:13:22 +0200

    Zmiana widoku "discipline"
    Edycja układu widoku 'discipline"

--- admin/tmpl/disciplines/default.php
+++ admin/tmpl/disciplines/default.php
@@ -23,17 +23,23 @@ use Joomla\CMS\Language\Text;
 		<table class="table table-striped table-hover">
 			<thead>
 				<tr>
-					<th><?php echo HTMLHelper::_('grid.checkall'); ?></th>
-					<th>Status</th>
-					<th>Title</th>
-					<th>ID</th>
+					<td class="w-1 text-center">
+									<?php echo HTMLHelper::_('grid.checkall'); ?>
+					</td>
+					<th scope="col">
+									<?php echo Text::_('COM_SPORTOWIADA_HEADING_NAME'); ?>
+					</th>
+					<th scope="col" class="w-5 d-none d-md-table-cell">
+									<?php echo Text::_('JGRID_HEADING_ID'); ?>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach ($this->items as $i => $item) : ?>
 					<tr>
-						<td><?php echo HTMLHelper::_('grid.id', $i, $item->id); ?></td>
-						<td><?php echo $item->published; ?></td>
+						<td class="text-center">
+                                    <?php echo HTMLHelper::_('grid.id', $i, $item->id, false, 'cid', 'cb', $item->name); ?>
+						</td>
 						<td>
 							<a href="<?php echo Route::_('index.php?option=com_sportowiada&task=discipline.edit&id=' . $item->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->name); ?>">
 								<?php echo $this->escape($item->name); ?>
